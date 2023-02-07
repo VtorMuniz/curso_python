c86cb113bd2222f719119b5cd1e2bc570d30db7e
vik <victormuniz1998@gmail.com>
2023-02-06 22:23:44 -0300

    estudos

--- forca.py
+++ forca.py
@@ -2,7 +2,23 @@ def jogar():
     print("*********************************")
     print("Bem vindo ao jogo da Forca!")
     print("*********************************")
-    print('Fim jogo')
+palavra_secreta = 'banana'
+enforcou = False
+acertou = False
+chute = input("Qual Letra?:")
+chute = chute.lower()
+index= 0
+for letra in palavra_secreta:
+    if(chute == letra):
+        print(f'Encontrei a letra {letra} na posição {index}')
+    index = index +1
+    print(index)
+
+    
+
+#while(not enforcou and not acertou):
+#  print('Jogando ....')
+
 
 if(__name__ == "__main__"):
     jogar()
