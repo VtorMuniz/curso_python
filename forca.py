172846baf6a73f4681b5c323d07ae56ede693420
vik <victormuniz1998@gmail.com>
2023-02-06 22:35:29 -0300

    iterando letras

--- forca.py
+++ forca.py
@@ -5,20 +5,18 @@ def jogar():
 palavra_secreta = 'banana'
 enforcou = False
 acertou = False
-chute = input("Qual Letra?:")
-chute = chute.lower()
-index= 0
-for letra in palavra_secreta:
-    if(chute == letra):
-        print(f'Encontrei a letra {letra} na posição {index}')
-    index = index +1
-    print(index)
+
+while(not enforcou and not acertou):
+    chute = input("Qual Letra?:")
+    chute = chute.lower()
+    index= 0
+    for letra in palavra_secreta:
+        if(chute == letra):
+            print('Encontrei a letra {} na posição {}'.format(letra,index))
+        index = index +1
+    print("Jogando")    
 
     
-
-#while(not enforcou and not acertou):
-#  print('Jogando ....')
-
 
 if(__name__ == "__main__"):
     jogar()
